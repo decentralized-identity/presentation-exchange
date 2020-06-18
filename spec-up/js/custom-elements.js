f21c1c382d9367b7c52b93473c7bd0dc34e44b3a
Daniel Buchner <danieljb2@gmail.com>
2020-06-18 16:46:40 -0700

    Merge changes

--- spec-up/js/custom-elements.js
+++ spec-up/js/custom-elements.js
@@ -1,4 +1,17 @@
 
+
+function delegateEvent(type, selector, fn, options = {}){
+  return (options.container || document).addEventListener(type, e => {
+    let node = e.target;
+    let match = node.matches(selector);
+    if (!match) while (!node.parentElement) {
+      if (node.parentElement.matches(selector)) { match = node; break; }
+      node = node.parentElement;
+    }
+    if (match) fn.call(node, e, node);
+  }, options);
+}
+
 customElements.define('slide-panels', class SidePanels extends HTMLElement {
   static get observedAttributes() {
     return ['open'];
@@ -79,3 +92,44 @@ customElements.define('detail-box', class DetailBox extends HTMLElement {
   }
 });
 
+let domReady = new Promise(resolve => {
+  document.addEventListener('DOMContentLoaded', e => resolve())
+});
+
+customElements.define('tab-panels', class TabPanels extends HTMLElement {
+  constructor() {
+    super();
+    delegateEvent('click', 'tab-panels > nav > *', (e, delegate) => {
+      let nav = delegate.parentElement;
+      if (nav.parentElement === this) {
+        this.setAttribute('selected-index', Array.prototype.indexOf.call(nav.children, delegate))
+      }
+    }, { container: this, passive: true });
+  }
+  static get observedAttributes() {
+    return ['selected-index'];
+  }
+  attributeChangedCallback(attr, last, current) {
+    domReady.then(() => {
+      switch(attr) {  
+        case 'selected-index':
+          let index = current || 0;
+          let nav = this.querySelector('nav');
+          if (nav.parentElement === this) {
+            let tabs = nav.children;
+            let selected = tabs[index];
+            for (let tab of tabs) tab.removeAttribute('selected');
+            if (selected) selected.setAttribute('selected', '');
+            let panel = Array.prototype.filter.call(this.children, node => {
+              if (node.tagName === 'SECTION') {
+                node.removeAttribute('selected');
+                return true;
+              }
+            })[index];
+            if (panel) panel.setAttribute('selected', '');
+          }
+          break;
+      }
+    });
+  }
+});
