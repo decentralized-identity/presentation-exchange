e83d071b6cf6c5aa54184c483f73b73fdc1a0579
George Aristy <george.aristy@securekey.com>
2021-03-12 21:05:59 -0500

    fix: definitions JSON Schema
    * hoisted definition to the schema's top level
    * require `minItems: 1` for the input_descriptors array
    * require `minItems: 1` for the "fields" array
    * fix: `max` must be greater than 0 and greater than `min`
    
    Signed-off-by: George Aristy <george.aristy@securekey.com>

--- test/presentation-definition/test.js
+++ test/presentation-definition/test.js
@@ -8,7 +8,7 @@ describe('Presentation Definition', function () {
     it('should validate the basic example object using JSON Schema Draft 7', function () {
       const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/basic_example.json'));
-      const jv = new ajv({allErrors: true});
+      const jv = new ajv({allErrors: true, $data: true});
       const validate = jv.compile(schema);
       const valid = validate(data);
 
@@ -19,7 +19,7 @@ describe('Presentation Definition', function () {
     it('should validate the single group example object using JSON Schema Draft 7', function () {
       const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/single_group_example.json'));
-      const jv = new ajv({allErrors: true});
+      const jv = new ajv({allErrors: true, $data: true});
       const validate = jv.compile(schema);
       const valid = validate(data);
 
@@ -30,7 +30,7 @@ describe('Presentation Definition', function () {
     it('should validate the multi group example object using JSON Schema Draft 7', function () {
       const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/multi_group_example.json'));
-      const jv = new ajv({allErrors: true});
+      const jv = new ajv({allErrors: true, $data: true});
       const validate = jv.compile(schema);
       const valid = validate(data);
 
@@ -41,7 +41,7 @@ describe('Presentation Definition', function () {
     it('should validate the format example object using JSON Schema Draft 7', function () {
       const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/format_example.json'));
-      const jv = new ajv({allErrors: true});
+      const jv = new ajv({allErrors: true, $data: true});
       const validate = jv.compile(schema);
       const valid = validate(data);
 
@@ -52,7 +52,7 @@ describe('Presentation Definition', function () {
     it('should validate the input description sample example object using JSON Schema Draft 7', function () {
       const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/input_descriptors_example.json'));
-      const jv = new ajv({allErrors: true});
+      const jv = new ajv({allErrors: true, $data: true});
       const validate = jv.compile(schema);
       const valid = validate(data);
 
@@ -63,7 +63,7 @@ describe('Presentation Definition', function () {
     it('should validate the input description id tokens example object using JSON Schema Draft 7', function () {
       const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/input_descriptor_id_tokens_example.json'));
-      const jv = new ajv({allErrors: true});
+      const jv = new ajv({allErrors: true, $data: true});
       const validate = jv.compile(schema);
       const valid = validate(data);
 
@@ -74,7 +74,7 @@ describe('Presentation Definition', function () {
     it('should validate the VC expiration example object using JSON Schema Draft 7', function () {
       const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/VC_expiration_example.json'));
-      const jv = new ajv({allErrors: true});
+      const jv = new ajv({allErrors: true, $data: true});
       const validate = jv.compile(schema);
       const valid = validate(data);
 
@@ -85,7 +85,7 @@ describe('Presentation Definition', function () {
     it('should validate the VC revocation example object using JSON Schema Draft 7', function () {
       const schema = JSON.parse(fs.readFileSync(__dirname + '/schema.json'));
       const data = JSON.parse(fs.readFileSync(__dirname + '/VC_revocation_example.json'));
-      const jv = new ajv({allErrors: true});
+      const jv = new ajv({allErrors: true, $data: true});
       const validate = jv.compile(schema);
       const valid = validate(data);
 
